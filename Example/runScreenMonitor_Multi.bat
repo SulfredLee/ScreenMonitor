845d63def1afbd56bb93008f9d9eb35b7e6a1360
SulfredLee <sflee1112@gmail.com>
2017-08-15 09:29:54 +0800

    Speed up directx method to 4x FPS by increasing pipe line number

--- Example/runScreenMonitor_Multi.bat
+++ Example/runScreenMonitor_Multi.bat
@@ -1,2 +1,2 @@
-'Usage: ScreenMonitor.exe -M[Single or Multi] -t Threshold(percentage, mask is 3x3)
-.\ScreenMonitor.exe -m Multi -t 30
+'Usage: ScreenMonitor.exe -M[Single or Multi] -t Threshold(percentage, mask is 3x3) -n NumOfPipeLine
+.\ScreenMonitor.exe -m Multi -t 30 -n 2
--- ScreenMonitor/ScreenMonitor.cpp
+++ ScreenMonitor/ScreenMonitor.cpp
@@ -19,59 +19,79 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	int nRetCode = 0;
 
-	if (argc < 5)
+	if (argc < 7)
 	{
 		nRetCode = 1;
 		std::cout << "Error:\n"
-			<< "Usage: ScreenMonitor.exe -m [Single|Multi]-t Threshold(percentage, mask is 3x3)\n";
+			<< "Usage: ScreenMonitor.exe -m [Single|Multi]-t Threshold(percentage, mask is 3x3) -n NumOfPipeLine\n";
 		return nRetCode;
 	}
 
 	ParsingCMD cmdPar;
-	cmdPar.Set("-t"); cmdPar.Set("-m");
+	cmdPar.Set("-t"); cmdPar.Set("-m"); cmdPar.Set("-n");
 	cmdPar.DoParsing(argc, argv);
 	double dThreshold = std::stod(cmdPar["-t"]); // threshold is percentage
+	int nNumOfPipeline = std::stoi(cmdPar["-n"]);
 	bool bIsSingleMon = cmdPar["-m"] == "Single" ? true : false;
 
-	std::shared_ptr<CThreadPool_Simple_Std> shr_ptrThreadPool(std::make_shared<CThreadPool_Simple_Std>());
+	if (nNumOfPipeline == 0)
+	{
+		nRetCode = 1;
+		std::cout << "Error:\n"
+			<< "Usage: Number of Pipeline cannot be zero\n";
+		return nRetCode;
+	}
 
-	//std::shared_ptr<CImageGreper_DirectX9> ImageGreper(std::make_shared<CImageGreper_DirectX9>());
-	//std::shared_ptr<CImageGreper_Win32> ImageGreper(std::make_shared<CImageGreper_Win32>());
-	//std::shared_ptr<CImageGreper_Win32MultipleScreen> ImageGreper(std::make_shared<CImageGreper_Win32MultipleScreen>());
-	std::shared_ptr<CImageGreper> ImageGreper;
-	if (bIsSingleMon)
+	std::shared_ptr<CThreadPool_Simple_Std> shr_ptrThreadPool(std::make_shared<CThreadPool_Simple_Std>());
+	std::vector<std::shared_ptr<CImageGreper> > vecImageGrepers;
+	std::vector<std::shared_ptr<CImageSelector_TaskMaker> > vecImageSelector_TaskMakers;
+	std::vector<std::shared_ptr<CImageSaver> > vecImageSavers;
+
+
+	// Handle Image Grepers
+	for (int i = 0; i < nNumOfPipeline; i++)
 	{
-		ImageGreper = std::make_shared<CImageGreper_DirectX9>();
+		if (bIsSingleMon)
+		{
+			vecImageGrepers.emplace_back(std::make_shared<CImageGreper_DirectX9>());
+		}
+		else
+		{
+			vecImageGrepers.emplace_back(std::make_shared<CImageGreper_Win32MultipleScreen>());
+		}
+		vecImageGrepers.back()->InitComponent(CImageGreper_Config(i + 1));
 	}
-	else
-	{
-		ImageGreper = std::make_shared<CImageGreper_Win32MultipleScreen>();
-	}
-	ImageGreper->InitComponent(CImageGreper_Config(1));
+	
 	// Select region of interest
 	CROISelector ROISelector;
 	std::vector<cv::Rect> vecROIs;
 	std::vector<int> vecIDs;
-	ROISelector.GetROI(ImageGreper->TakeAScreenShot(), vecROIs, vecIDs);
+	ROISelector.GetROI(vecImageGrepers.front()->TakeAScreenShot(), vecROIs, vecIDs);	
 
-	// Image update checking
-	std::shared_ptr<CImageSelector_TaskMaker> ImageSelector_TaskMaker(std::make_shared<CImageSelector_TaskMaker>());
-	ImageSelector_TaskMaker->InitComponent(CImageSelector_TaskMaker_Config(vecROIs,
-		vecIDs,
-		dThreshold,
-		shr_ptrThreadPool));
 
-	// Handle Image saver
-	std::shared_ptr<CImageSaver> ImageSaver(std::make_shared<CImageSaver>());
-	ImageSaver->InitComponent(CImageSaver_Config(".\\"));
+	for (int i = 0; i < nNumOfPipeline; i++)
+	{
+		// Handle Image update checking
+		vecImageSelector_TaskMakers.emplace_back(std::make_shared<CImageSelector_TaskMaker>());
+		vecImageSelector_TaskMakers.back()->InitComponent(CImageSelector_TaskMaker_Config(vecROIs,
+			vecIDs,
+			dThreshold,
+			shr_ptrThreadPool));
 
-	// Object connection
-	ImageGreper->AddObserver(ImageSelector_TaskMaker);
-	ImageSelector_TaskMaker->AddObserver(ImageSaver);
+		// Handle Image saver
+		vecImageSavers.emplace_back(std::make_shared<CImageSaver>());
+		vecImageSavers.back()->InitComponent(CImageSaver_Config(".\\"));
+
+		// Object connection
+		vecImageGrepers[i]->AddObserver(vecImageSelector_TaskMakers[i]);
+		vecImageSelector_TaskMakers[i]->AddObserver(vecImageSavers[i]);		
+	}
 
 	// Start application
-	ImageGreper->StartThread();
-
+	for (int i = 0; i < nNumOfPipeline; i++)
+	{
+		vecImageGrepers[i]->StartThread();
+	}
 
 	while (true)
 	{
